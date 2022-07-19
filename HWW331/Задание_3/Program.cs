7b91b3a3a401459c408c0a976c768074fe15ccab
Timurgess <timurgess@yandex.ru>
2022-07-19 17:47:51 +0300

    Задание_3

---
+++ HWW331/Задание_3/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+int numberA = 7;
+int numberB = 9;
+int [] getCubeOfNumber (int n)
+{
+    int[] arr = new int [n];
+
+    for (int i = 1; i !=n+1; i++)
+    {
+        arr[i-1] = i * i * i;
+    }
+    return arr;
+}
+
+Console.WriteLine("{0} -> {1}",numberA, string.Join(",", getCubeOfNumber(numberA)));
+Console.WriteLine("{0} -> {1}",numberB, string.Join(",", getCubeOfNumber(numberB)));
